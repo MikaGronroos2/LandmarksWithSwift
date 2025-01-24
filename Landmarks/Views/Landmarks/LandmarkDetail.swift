bfe28d2b52438f030a859bb255967aed295b0554
mikagronroos2 <mika.gronroos2@metropolia.fi>
2025-01-24 14:56:25 +0200

    Updating Badges and assets andlearning graphical drawing

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
+        
+        @Bindable var modelData = modelData
         ScrollView {
             
             MapView(coordinate: landmark.locationCoordinate)
@@ -19,8 +26,11 @@ struct LandmarkDetail: View {
             CircleImage(image: landmark.image).offset(y: -130).padding(.bottom, -130)
             
             VStack(alignment: .leading){
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 
                 HStack {
                     Text(landmark.park)
@@ -42,5 +52,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+    let modelData = ModelData()
+
+    return LandmarkDetail(landmark: ModelData().landmarks[0]).environment(modelData)
 }
