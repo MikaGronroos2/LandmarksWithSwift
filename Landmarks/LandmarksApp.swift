c8d63f3b46578b80699cc8fd38d2633fd2bf7cca
mikagronroos2 <mika.gronroos2@metropolia.fi>
2025-01-24 10:00:31 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mika Grönroos on 24.1.2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
